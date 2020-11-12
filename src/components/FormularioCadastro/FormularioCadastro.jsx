205e22bbc013d32a82d2b578f9a531f4267f656c
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-11-12 20:50:29 -0300

    Assincronicidade e validação

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,22 +2,24 @@ import React, {useState} from 'react';
 import { TextField, Button, FormControlLabel, Switch } from '@material-ui/core';
 
 function FormularioCadastro() {
-    const [nome,setNome] = useState("Higor");
+    const [nome,setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
     
     return (
         <form 
             onSubmit={(event) => {
             event.preventDefault();
-            console.log(nome);
+            console.log(nome,sobrenome);
         }}>
             <TextField
                 value={nome}
                 onChange={(event) => {
-                    setNome(event.target.value);
-                    
-                    if(nome.length >= 3){
-                        setNome(nome.substr(0,3));
+                    let tmpNome = event.target.value;                    
+                    if(tmpNome.length >= 3){
+                        tmpNome = tmpNome.substr(0,3);
                     }
+                    setNome(tmpNome);
+
                 }}
                 id="nome"
                 label="Nome"
@@ -26,6 +28,10 @@ function FormularioCadastro() {
             />
 
             <TextField
+                value={sobrenome}
+                onChange={(event) => {
+                    setSobrenome(event.target.value);
+                }}
                 id="sobrenome"
                 label="Sobrenome"
                 variant="outlined"
